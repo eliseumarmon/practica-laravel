bdc5114994cb520f23fbcc5c1bac9ec4ceabb90a
Eliseu Martínez <elimarmon@ieseljust.com>
2025-11-29 18:08:40 +0100

    Creado componente para pagina servicios

--- resources/views/landing/services.blade.php
+++ resources/views/landing/services.blade.php
@@ -5,4 +5,16 @@
 @endsection
 @section("main")
     <p>Descripción de nuestros servicios.</p>
+    @component("components.card")
+        @slot("title", "Servicio 1")
+        @slot("content", "Descripción del servicio 1")
+    @endcomponent
+    @component("components.card")
+        @slot("title", "Servicio 2")
+        @slot("content", "Descripción del servicio 2")
+    @endcomponent
+    @component("components.card")
+        @slot("title", "Servicio 3")
+        @slot("content", "Descripción del servicio 3")
+    @endcomponent
 @endsection
