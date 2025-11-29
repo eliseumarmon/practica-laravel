1a7ca92ca7de752e55a1440b8f64f1e539f07865
Eliseu Martínez <elimarmon@ieseljust.com>
2025-11-29 18:01:44 +0100

    Convertido nav a partials

--- resources/views/_layouts/landing.blade.php
+++ resources/views/_layouts/landing.blade.php
@@ -8,14 +8,7 @@
 </head>
 <body>
     <header>
-        <nav>
-            <ul>
-                <li><a href="{{ route("home") }}">Inicio</a></li>
-                <li><a href="{{ route("about") }}">Sobre nosotros</a></li>
-                <li><a href="{{ route("services") }}">Servicios</a></li>
-                <li><a href="{{ route("contact") }}">Contacto</a></li>
-            </ul>
-        </nav>
+        @include("_partials.nav")
         @yield("header")
     </header>
     <main>
