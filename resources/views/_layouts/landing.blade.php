9255320a06cdb0face8c181d09e5d92f58d89d1b
Eliseu Martínez <elimarmon@ieseljust.com>
2025-11-29 17:59:33 +0100

    Añadir nav a layout base

--- resources/views/_layouts/landing.blade.php
+++ resources/views/_layouts/landing.blade.php
@@ -8,6 +8,14 @@
 </head>
 <body>
     <header>
+        <nav>
+            <ul>
+                <li><a href="{{ route("home") }}">Inicio</a></li>
+                <li><a href="{{ route("about") }}">Sobre nosotros</a></li>
+                <li><a href="{{ route("services") }}">Servicios</a></li>
+                <li><a href="{{ route("contact") }}">Contacto</a></li>
+            </ul>
+        </nav>
         @yield("header")
     </header>
     <main>
